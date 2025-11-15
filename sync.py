8f61f51609131bdd4b68059218ba88879263edc3
shantanu <shantanu.chikerur20@gmail.com>
2025-11-15 18:48:00 +0530

    Changes

--- sync.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True, args=["--no-sandbox"])
-    page = browser.new_page()
-    page.goto("https://demo.automationtesting.in/Index.html")
-    print("Page title: ", page.title())
-    page.screenshot(path="demo.png")
-    browser.close()
